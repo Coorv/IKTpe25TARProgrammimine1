8fbd0c367d53f5459c70e8732301345dac2b2ade
Coorv <liisinurk7@gmail.com>
2026-02-03 15:12:59 +0200

    Dictionary

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dictionary ja foreach");
+
+            //var on muutuja
+            
+            var domains = new Dictionary<string, string>()
+            {
+                { "fi", "Finland"},
+                { "se", "Sweden"},
+                { "de", "Germany"},
+                { "fr", "France"},
+                { "es", "Spain"},
+            };
+            //kasutate foreachi ja juurde tuleb lisada kolmas rida
+            int counter = 1;
+            foreach (var element in domains)
+            {
+                Console.WriteLine(element.Key + " - " + element.Value +  " - " + counter);
+                counter++;
+            }
+        }
+    }
+}
