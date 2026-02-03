b1599763deb0b0a536e654b059b0a85fbaa06068
Coorv <119966974+Coorv@users.noreply.github.com>
2026-02-03 15:22:57 +0200

    minmax

---
+++ MinMaxSum/MinMaxSum/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MinMaxSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("List numbrites");
+
+            int[] numbers = new int[10] { 1, 3, 14, 17, 6, 2, 8, 12, 24, 69 };
+            //max
+            Console.WriteLine(numbers.Max());
+            //min
+            Console.WriteLine(numbers.Min());
+            //sum
+            Console.WriteLine(numbers.Sum());
+            //average
+            Console.WriteLine(numbers.Average());
+        }
+    }
+}
