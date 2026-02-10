758bd5b0d21a748b520015eb9945a6fdef11ef96
Opilane TTHK <119966974+Coorv@users.noreply.github.com>
2026-02-10 15:09:51 +0200

    EnumProject

--- EnumProject/EnumProject/Program.cs
+++ EnumProject/EnumProject/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace EnumProject
+﻿using System.ComponentModel;
+
+namespace EnumProject
 {
     internal class Program
     {
@@ -7,8 +9,52 @@
             Console.WriteLine("Enum");
 
             //kasutate emumit Weekdays ja tahaks näha Friday.
-
             Console.WriteLine(Weekdays.Friday);
+
+            //tahame näha Friday numbrilist väärtust
+            Console.WriteLine((int)Weekdays.Friday);
+            //(int) - Castimine, ehk teisendab andmetüüpi
+            //juhul kui info võib kaduma minna ja ei näita soovitud tulemust
+            Console.WriteLine("---------------------");
+            //tehke uus enum Colors
+            //väärtused on 
+            //Red = 3,
+            //Green = 10,
+            //blue = 13,
+            //Yellow = 5,
+            //Black = 1,
+            //White = 8
+            //ühe värvi nimetuse peab konsoolis ära näitama.
+            string valik = "";
+            Console.WriteLine("What color do you chose?");
+            valik = Console.ReadLine();
+            switch (valik)
+            {
+                case "Red":
+                    Console.WriteLine((int)Colors.Red);
+                    Console.WriteLine(Colors.Red);
+                    break;
+                case "Green":
+                    Console.WriteLine((int)Colors.Green);
+                    Console.WriteLine(Colors.Green);
+                    break;
+                case "blue":
+                    Console.WriteLine((int)Colors.blue);
+                    Console.WriteLine(Colors.blue);
+                    break;
+                case "Yellow":
+                    Console.WriteLine((int)Colors.Yellow);
+                    Console.WriteLine(Colors.Yellow);
+                    break;
+                case "Black":
+                    Console.WriteLine((int)Colors.Black);
+                    Console.WriteLine(Colors.Black);
+                    break;
+                case "White":
+                    Console.WriteLine((int)Colors.White);
+                    Console.WriteLine(Colors.White);
+                    break;
+            }
         }
 
         enum Weekdays
@@ -20,6 +66,15 @@
             Friday,
             Saturday,
             Sunday
+        }
+        enum Colors
+        {
+            Red = 3,
+            Green = 10,
+            blue = 13,
+            Yellow = 5,
+            Black = 1,
+            White = 8
         }
     }
 }
