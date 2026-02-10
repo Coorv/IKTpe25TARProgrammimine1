8e60484d02793214995c9f6fd1f539d8b48cc024
Opilane TTHK <119966974+Coorv@users.noreply.github.com>
2026-02-10 14:43:34 +0200

    enum kuvamine

---
+++ EnumProject/EnumProject/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EnumProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enum");
+
+            //kasutate emumit Weekdays ja tahaks näha Friday.
+
+            Console.WriteLine(Weekdays.Friday);
+        }
+
+        enum Weekdays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
