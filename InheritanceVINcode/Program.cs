33b690319905801b16bb2c943874645f5e157a4a
Coorv <119966974+Coorv@users.noreply.github.com>
2026-03-12 11:31:18 +0200

    Ülesanne

---
+++ InheritanceVINcode/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace InheritanceVINcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Teha pärilus
+            //On olemas class nimega Machine
+            //See pärib Cars classi
+            //saab sisestada masina numbri
+            //konsool annab vastuse: Edukalt sisestatud
+            // VIN kood: VIN koodi nr
+            Console.WriteLine("sisesta VIN kood: ");
+            int vinCode = Convert.ToInt32(Console.ReadLine());
+
+            Machine machine = new Machine();
+            machine.SetVinCode(vinCode);
+
+            Console.WriteLine("Vin Code is: " + machine.GetVinCode());
+        }
+    }
+    class Machine : Car
+    {
+        public int GetVinCode()
+        {
+            return vin;
+        }
+    }
+    class Car
+    {
+        public void SetVinCode(int vinCode)
+        {
+            vin = vinCode;
+        }
+        protected int vin;
+    }
+}
