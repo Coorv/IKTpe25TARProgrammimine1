9b2fc2288cbb57c4b2b2cf78fea99f9142e43e8a
Coorv <119966974+Coorv@users.noreply.github.com>
2026-03-12 11:06:54 +0200

    Pärimine

---
+++ Inheritance/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert to numbers: ");
+            Console.WriteLine("First number: ");
+            int firstNr = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Second number: ");
+            int secondNr = Convert.ToInt32(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetWidth(firstNr);
+            rectangle.SetHeight(secondNr);
+            Console.WriteLine("Total area: {0}", rectangle.GetArea());
+        }
+    }
+
+    class Shape
+    {
+        public void SetWidth(int w)
+        {
+            width = w;
+        }
+
+        public void SetHeight(int h)
+        {
+            height = h;
+        }
+
+        protected int width;
+        protected int height;
+    }
+
+    //koolon tähendab pärimist
+    //läbi pärimise saame kasutada muutujaid width ja height Shape classis
+    //ning neid ei pea siis defineerima Rectangle classis
+    class Rectangle : Shape
+    {
+        public int GetArea()
+        {
+            //return tagastab info selles meetodis toimunud loogika kohta
+            return width * height;
+        }
+    }
+}
